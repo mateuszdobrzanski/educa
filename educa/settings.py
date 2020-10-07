47c8634f12a77389e80ddcfce7d5a5fad1e0e28d
mdob <mateuszdobrzanski@outlook.com>
2020-10-07 13:06:07 +0200

    init app at settings.py

--- educa/settings.py
+++ educa/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # import projects apps
+    'courses'
+    
+    
+    # default django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
