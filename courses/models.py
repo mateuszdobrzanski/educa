d2d75ef71d202552a23423328314bdf6d054570d
mdob <mateuszdobrzanski@outlook.com>
2020-10-07 22:39:29 +0200

    add objects from OrderList to order data

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 
 # General category of course
@@ -44,8 +45,14 @@ class Module(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
 
+    # the order depends on the course
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
+
     def __str__(self):
-        return self.title
+        return '{}. {}'.format(self.order, self.title)
 
 
 # this class/model can handle different types for each module
@@ -65,6 +72,12 @@ class Content(models.Model):
     object_id = models.PositiveIntegerField()
     # get bound object using object_id and content_type
     item = GenericForeignKey('content_type', 'object_id')
+
+    # the order depends on the module
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 # abstract model for all types to handle data
