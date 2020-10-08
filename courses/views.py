7e56aa07d31bce246907fbbd290dda9d1c0f7db2
mdob <mateuszdobrzanski@outlook.com>
2020-10-08 12:44:52 +0200

    in views.py added possibility to view all courses by user

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Course
 
-# Create your views here.
+
+# this inherit LisView view
+class MangeCoursesListView(ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+
+    # override default queryset to get all courses for current user
+    def get_queryset(self):
+        qs = super(MangeCoursesListView, self).get_queryset()
+        return qs.filter(owner=self.request.user)
